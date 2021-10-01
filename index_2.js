0525dd6432473d02966744214a0e1f9f9c512a45
Aakash Tyagi <aakashtyagi354@gmail.com>
2021-10-01 15:11:18 +0530

    project completed

--- index_2.js
+++ index_2.js
@@ -1,1 +1,17 @@
-const color = ["green",  "red", "rgba(133,122,200)","#f15025"];
+ 
+
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const  colors = ['red','green','blue','pink','yellow','violet','purple'];
+const color = document.querySelector('.color');
+
+body.style.backgroundColor = 'purple';
+button.addEventListener('click',changeBackground);
+
+function changeBackground(){
+     const colorIndex = parseInt(Math.random()*colors.length)
+
+    
+    body.style.backgroundColor = colors[colorIndex];
+    color.textContent = colors[colorIndex];
+}
